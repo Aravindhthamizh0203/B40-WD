e851fbb61d490cdac9e991844b6898cac9f3f6f9
Aravindh A <113359255+Aravindhthamizh0203@users.noreply.github.com>
2022-09-19 12:59:32 +0530

    day  2
    day 2

---
+++ day 2 .js
@@ -0,0 +1,8 @@
+var json = {
+  "name": "Aravindh A",
+  "age": 23,
+  "date of birth" : 18/03/1999,
+  "father name" : "ashokan",
+  "qualification" : "B-Tech Food Technology",
+  "phone number" : 8940754429,
+}
